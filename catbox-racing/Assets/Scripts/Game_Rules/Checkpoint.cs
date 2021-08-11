3e121151b7c24a17394da9700c605987a55dabe9
John Paulo <johnpaulo0401@gmail.com>
2021-08-11 20:19:24 -0300

    Adding checkpoint system
    Adicionando o sistema de checkpoints.

---
+++ catbox-racing/Assets/Scripts/Game_Rules/Checkpoint.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    private TrackCheckpoints trackCheckpoints;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.TryGetComponent<Player>(out Player player))
+        {
+            trackCheckpoints.KartTroughCheckpoint(this, other.transform);
+        }
+    }
+
+    public void SetTrackCheckpoints(TrackCheckpoints trackCheckpoints)
+    {
+        this.trackCheckpoints = trackCheckpoints;
+    }
+
+
+}
---
+++ catbox-racing/Assets/Scripts/Game_Rules/TrackCheckpoints.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackCheckpoints : MonoBehaviour
+{
+    [SerializeField] private List<Transform> kartTransformList;
+
+    private List<Checkpoint> checkpointList;
+    private List<int> nextCheckpointIndexList;
+
+    void Awake()
+    {
+        Transform checkpointsTransform = transform.Find("Checkpoints");
+
+        checkpointList = new List<Checkpoint>();
+        foreach (Transform checkpointTransform in checkpointsTransform)
+        {
+            Checkpoint checkpoint = checkpointTransform.GetComponent<Checkpoint>();
+
+            checkpoint.SetTrackCheckpoints(this);
+
+            checkpointList.Add(checkpoint);
+        }
+
+        nextCheckpointIndexList = new List<int>();
+        foreach (Transform kartTransform in kartTransformList)
+        {
+            nextCheckpointIndexList.Add(0);
+        }
+    }
+
+    public void KartTroughCheckpoint(Checkpoint checkpoint, Transform kartTransform)
+    {
+        int nextCheckpointIndex = nextCheckpointIndexList[kartTransformList.IndexOf(kartTransform)];
+        if (checkpointList.IndexOf(checkpoint) == nextCheckpointIndex)
+        {
+            nextCheckpointIndexList[kartTransformList.IndexOf(kartTransform)] = (nextCheckpointIndex + 1) % checkpointList.Count;
+        }
+    }
+}

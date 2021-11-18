74ef90d94f39cfe43080b3c2f95d3dfb42121180
John Paulo <johnpaulo0401@gmail.com>
2021-11-18 15:12:20 -0300

    Start the programation of the kart positions of the track

---
+++ catbox-racing/Assets/Scripts/GameRules/RaceManager/KartsPositions.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KartsPositions : MonoBehaviour
+{
+    [SerializeField] private KartController[] kartPositions;
+
+    void Start()
+    {
+        //Debug.Log(kartPositions[0].Length);
+    }
+
+    void Update()
+    {
+        
+    }
+}

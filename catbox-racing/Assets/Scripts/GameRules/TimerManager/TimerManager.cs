b5b80da3d174b1d26044cff111cd1631d4ba0444
John Paulo <johnpaulo0401@gmail.com>
2021-12-02 10:10:29 -0300

    Starting the logic of the track selection

--- catbox-racing/Assets/Scripts/GameRules/TimerManager/TimerManager.cs
+++ catbox-racing/Assets/Scripts/GameRules/TimerManager/TimerManager.cs
@@ -12,6 +12,8 @@ public class TimerManager : MonoBehaviour
     private int secondCount;
     private float millisecondCount;
     private string millisecondDisplay;
+    private float rawTime;
+    private float rawTimePlayer;
 
     private void Awake()
     {
@@ -20,35 +22,60 @@ public class TimerManager : MonoBehaviour
 
     private void LapComplete_OnPlayerLapDone(object sender, System.EventArgs e)
     {
-        //logica de salvar o tempo e comparar com o tempo anterior.
+        rawTimePlayer = PlayerPrefs.GetFloat("RawTime");
+        if (rawTime <= rawTimePlayer)
+        {
+            if (secondCount <= 9)
+                secondBox.GetComponent<TextMeshProUGUI>().text = "0" + secondCount + ".";
+            else
+                secondBox.GetComponent<TextMeshProUGUI>().text = "" + secondCount + ".";
+
+            if (minuteCount <= 9)
+                minuteBox.GetComponent<TextMeshProUGUI>().text = "0" + minuteCount + ".";
+            else
+                minuteBox.GetComponent<TextMeshProUGUI>().text = "" + minuteCount + ".";
+
+            millisecondBox.GetComponent<TextMeshProUGUI>().text = "" + millisecondCount;
+        }
+
+        PlayerPrefs.SetInt("MinSave", minuteCount);
+        PlayerPrefs.SetInt("SecSave", secondCount);
+        PlayerPrefs.SetFloat("MilliSave", millisecondCount);
+        PlayerPrefs.SetFloat("RawTime", rawTime);
+
+        millisecondCount = 0;
+        secondCount = 0;
+        millisecondCount = 0;
+        rawTime = 0;
     }
 
     private void Update()
     {
         millisecondCount += Time.deltaTime * 10;
+        rawTime += Time.deltaTime;
         millisecondDisplay = millisecondCount.ToString("F0");
         millisecondBox.GetComponent<TextMeshProUGUI>().text = "" + millisecondDisplay;
 
-        if(millisecondCount >= 10)
+        if (millisecondCount >= 10)
         {
             millisecondCount = 0;
             secondCount += 1;
         }
 
-        if(secondCount <= 9)       
-            secondBox.GetComponent<TextMeshProUGUI>().text = "0" + secondCount + ".";       
-        else      
-            secondBox.GetComponent<TextMeshProUGUI>().text = "" + secondCount + ".";       
+        if (secondCount <= 9)
+            secondBox.GetComponent<TextMeshProUGUI>().text = "0" + secondCount + ".";
+        else
+            secondBox.GetComponent<TextMeshProUGUI>().text = "" + secondCount + ".";
 
         if (secondCount >= 60)
         {
-            millisecondCount = 0;
+            secondCount = 0;
             minuteCount += 1;
         }
 
-        if (minuteCount <= 9)      
-            minuteBox.GetComponent<TextMeshProUGUI>().text = "0" + minuteCount + ":";      
-        else     
-            minuteBox.GetComponent<TextMeshProUGUI>().text = "" + minuteCount + ":";        
+        if (minuteCount <= 9)
+            minuteBox.GetComponent<TextMeshProUGUI>().text = "0" + minuteCount + ":";
+        else
+            minuteBox.GetComponent<TextMeshProUGUI>().text = "" + minuteCount + ":";
     }
 }
---
+++ catbox-racing/Assets/Scripts/UI/Menu/MenuTrackSelection.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MenuTrackSelection : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI minuteDisplay;
+    [SerializeField] private TextMeshProUGUI secondDisplay;
+    [SerializeField] private TextMeshProUGUI millisecondDisplay;
+    private int minuteCount;
+    private int secondCount;
+    private float milliCount;
+
+    void Start()
+    {
+        minuteCount = PlayerPrefs.GetInt("MinSave");
+        secondCount = PlayerPrefs.GetInt("SecSave");
+        milliCount = PlayerPrefs.GetInt("MilliSave");
+
+        minuteDisplay.text = "" + minuteCount + ":";
+        secondDisplay.text = "" + secondCount + ".";
+        millisecondDisplay.text = "" + milliCount;
+    }
+}
